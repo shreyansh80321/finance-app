bacdd34755490ef45b63b2378d156dbed315f776
SHREYANSH80321 <SIBERSUPER1122@GMAIL.com>
2025-08-25 17:27:21 +0530

    final change

--- components/hero.jsx
+++ components/hero.jsx
@@ -42,9 +42,9 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="https://finance-app-gamma-ashen.vercel.app/">
             <Button size="lg" variant="outline" className="px-8">
-              improve
+              Github
             </Button>
           </Link>
         </div>
